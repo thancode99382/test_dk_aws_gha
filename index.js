ee6edf9b37a5cfbcb1ea286ca334529827c79b86
thancode99382 <dat99382@donga.edu.vn>
2025-10-02 16:44:00 +0700

    Initial deployment setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express + Docker + GitHub Actions + EC2 🚀");
+  res.send("I love you babe 😍");
 });
 
 app.get("/health", (req, res) => {
